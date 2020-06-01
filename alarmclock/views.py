915baa7d0545d76a71f6671eb61eeb410a19235d
pieru <aran0204.ambro@icloud.com>
2020-06-02 02:06:54 +0900

    最初の画面のベース

--- alarmclock/views.py
+++ alarmclock/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+import datetime
 
 # Create your views here.
+
+class AlarmClockClass(TemplateView):
+    template_name = 'alarmclock.html'
