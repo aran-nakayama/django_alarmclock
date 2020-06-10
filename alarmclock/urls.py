623c61dd338c24867456e41f187c2578000b6eee
pieru <aran0204.ambro@icloud.com>
2020-06-10 16:13:52 +0900

    mp3が読み込まれない

--- alarmclock/urls.py
+++ alarmclock/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import AlarmClockClass, WaitingPage
+from .views import AlarmClockClass, WaitingPage, SoundPage
 
 urlpatterns = [
-    path('alarmclock/', AlarmClockClass.as_view(), name = 'alarmclock'),
+    path('alarmclock/<int:pk>', AlarmClockClass.as_view(), name = 'alarmclock'),
+    #alarmclock/15 で更新できる。
     path('waiting/', WaitingPage.as_view(), name='waiting'),
+    path('sound/', SoundPage.as_view(), name= 'sound'),
 ]
--- alarmclock/views.py
+++ alarmclock/views.py
@@ -1,21 +1,20 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, CreateView, ListView
+from django.views.generic import TemplateView, CreateView, ListView, UpdateView
 import datetime
 from .models import TimeModel
 from django.urls import reverse_lazy
+
 # Create your views here.
 
-class AlarmClockClass(CreateView):
+class AlarmClockClass(UpdateView):
     template_name = 'alarmclock.html'
     model = TimeModel
     fields = ('hour', 'minute')
     success_url = reverse_lazy('waiting')
 
-
-
-
 class WaitingPage(ListView):
     template_name = 'waiting.html' 
     model = TimeModel
-    
-    
+
+class SoundPage(TemplateView):
+    template_name = 'sound.html'
